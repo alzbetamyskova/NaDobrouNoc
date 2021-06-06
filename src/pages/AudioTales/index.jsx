4d0efe2aa1b774ccece78cc608643f85e7608987
Alzbeta Myskova <betka.myskova@seznam.cz>
2021-06-06 21:53:24 +0200

    přidány pohádky, přidán footer do playeru

--- src/pages/AudioTales/index.jsx
+++ src/pages/AudioTales/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import AudioPlayer from '../../components/AudioPlayer/AudioPlayer';
 import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 import { PAGES } from '../../helpers';
 import unicorn from '../../components/AudioPlayer/img/unicorn.png';
 import data from '../../assets/database.json';
@@ -11,6 +12,7 @@ const AudioTales = () => {
       <Header page={PAGES.audiotales}/>
       <h3>Poslech pohádek</h3>
       <AudioPlayer tracks={data.audio}/>
+      <Footer fixedbottom={true}/>
     </div>
   );
 };
