eda70b6037c0b36f84491c349b38ff70cfab0a6e
Alzbeta Myskova <betka.myskova@seznam.cz>
2021-06-07 10:02:51 +0200

    přidáno logo, přidány pohádky, upraven úvod

--- src/components/Descriptions/index.jsx
+++ src/components/Descriptions/index.jsx
@@ -3,7 +3,11 @@ import './style.css';
 
 const Descriptions = () => {
   return(
-    <div className='descriptions'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos, eligendi. Tenetur atque ad ea, labore voluptas quasi in, corporis totam dolores fugiat porro vitae iusto voluptate accusamus omnis perferendis ducimus!</div>
+    <div className='descriptions'>
+      <p>Vítejte, drahé maminky, tatínkové, tetičky, starší sestry či bratrové, baičky i dědečkové ve webové aplikaci Na dobrou noc. Máte doma ratolesti či děti, které nemohou večer usnout? Přečtěte jim pohádku!</p>
+      <p>Můžete zvolit pohádku náhodnou, můžete si vybrat ze seznamu nebo si můžete pohádku najít podle klíčového slova. Dále můžete svým dětem či sourozencům pustit audio pohádku, či pohádku z videa, namluvenou.</p>
+      <p>Pojďme se do toho pustit společně, tlačítkem níže.</p>
+    </div>
   );
 };
 
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Menu from '../Menu';
 import './style.css';
+import moon from '../../assets/night.svg';
 
 const Header = ({page}) => {
   return(
     <div className='header'>
-      <h3 className='logo'>Na dobrou noc</h3>
+      <div className='logo'>
+        <img className='moon' src={moon}/>
+        <h3 className='textlogo'>Na dobrou noc</h3>
+      </div>
       <Menu page={page}/>
     </div>
   );
