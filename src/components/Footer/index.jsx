57940240a8b8c778b464c44b95b2e66b1ec99d86
Alzbeta Myskova <betka.myskova@seznam.cz>
2021-06-13 10:18:52 +0200

    práce na responzivním webu

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,7 +3,7 @@ import './style.css';
 
 const Footer = ({fixedbottom = false}) => {
   return(
-    <div className={`footer ${fixedbottom ? 'fixedbottom' : ''}`}>Vytvořeno v rámci Czechitas z.s., © Alžběta Myšková, 2021</div>
+    <div className={`footer ${fixedbottom ? 'fixedbottom' : ''}`}><p>Vytvořeno v rámci Czechitas z.s., © Alžběta Myšková, 2021</p></div>
   );
 };
 
